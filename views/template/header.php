0f70589549583bb19d249f906840fe43b051d660
Alexis Ferrer <alexisferrer60160@gmail.com>
2022-09-27 13:56:08 +0200

    ajout des fichiers requis pour la structure

---
+++ views/template/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
+    <script src="../../public/assets/js/script.js" defer></script>
+    <link rel="stylesheet" href="../../public/assets/css/style.css">
+    <title>Pineapple.lamanu</title>
+</head>
+<body>
