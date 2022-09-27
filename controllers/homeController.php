0c4c64dc5af75dea0e28d20f19ac225b86d9e5d1
Alexis Ferrer <alexisferrer60160@gmail.com>
2022-09-27 11:31:38 +0200

    Création du repo Github pour PineapplePhp

---
+++ controllers/homeController.php
@@ -0,0 +1,21 @@
+<?php 
+require_once(__DIR__.'/../config/config.php');
+
+
+// TRAITEMENT (IF ETC ETC ETC ETC ETC ETC ETC...........)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+include(__DIR__.'/../views/home.php');
