c5cc099180ddf65dcccd7461351a77c60e1de9b4
Alexis Ferrer <alexisferrer60160@gmail.com>
2022-09-29 09:04:38 +0200

    template header/footer + fichier ngrok

---
+++ controllers/homeController.php
@@ -0,0 +1,4 @@
+<?php
+    require_once(__DIR__.'/../views/template/header.php');
+    include(__DIR__.'/../views/home.php');
+    require_once(__DIR__.'/../views/template/footer.php');
--- views/template/header.php
+++ views/template/header.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
-    <script src="../../public/assets/js/script.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous" defer></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="../../public/assets/css/style.css">
-    <title>Pineapple.lamanu</title>
+    <title>Pineapple</title>
 </head>
+
 <body>
+    <div class="page">
+        <header class="header">
+            <img id="logo" src="../../public/assets/img/pineappleDark.png" alt="Logo Pineapple">
+            <h1 class="header-title">Pineapple</h1>
+            <div id="menu-button" class="close"></div>
+        </header>
+        <nav id="menu" class="">
+            <ul>
+                <li>Téléphone</li>
+                <li>Tablette</li>
+                <li>Pc Portable</li>
+                <li>Pc périphérique</li>
+                <li>Objets connectés</li>
+            </ul>
+        </nav>
+        <button id="switchMode">☀️</button>
